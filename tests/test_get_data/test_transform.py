0bc7376e08a6a325ec47b0dae3a857a49747d36a
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-09-01 21:59:26 +0200

    test empty comment selector

--- tests/test_get_data/test_transform.py
+++ tests/test_get_data/test_transform.py
@@ -5,11 +5,11 @@ from get_data import DB_questions
 def test_html_transform():
     post_in = [
         [15, "Post 1", 25, "<p><strong>Hello World</strong></p>"], 
-        [15, "Post 1", 25, "<!-- wp:tadv/classic-paragraph -->\n<p><strong>A versenyekről röviden</strong></p>\n<p>Nevezés:"]
+        [15, "Post 2", 25, "<!-- wp:tadv/classic-paragraph -->\n<p><strong>A versenyekről röviden</strong></p>\n<p>Nevezés:"]
         ]
     post_out = [
         [15, "Post 1", 25, "<p>[b]Hello World[/b]</p>"], 
-        [15, "Post 1", 25, "<!-- wp:tadv/classic-paragraph -->\n<p>[b]A versenyekről röviden[/b]</p>\n<p>Nevezés:"]
+        [15, "Post 2", 25, "<!-- wp:tadv/classic-paragraph -->\n<p>[b]A versenyekről röviden[/b]</p>\n<p>Nevezés:"]
         ]
     tr = DB_questions()
     test2 = tr.html_transform(post_in)
