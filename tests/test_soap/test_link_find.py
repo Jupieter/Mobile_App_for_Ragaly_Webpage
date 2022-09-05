204883808907a607f89ab6a79638c102e23abfbb
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-09-05 12:58:04 +0200

    link nonamed

--- tests/test_soap/test_link_find.py
+++ tests/test_soap/test_link_find.py
@@ -5,11 +5,11 @@ def test_link_find():
     # data_in = '<p><strong>Hello World</strong></p>'
     data_in = '''
         <p><strong>Hello World</strong></p> 
-        <p><a href="http://ragaly.hu/wp-content/uploads/2020/09/Plakat_2021-2.jpg">
+        <p><a href="http://adress.hu/wp-content/uploads/2020/09/Plakat_2021-2.jpg">
         <img class="size-large wp-image-42622 aligncenter" 
-        src="http://ragaly.hu/wp-content/uploads/2020/09/Plakat_2021-2-714x1024.jpg" 
+        src="http://adress.hu/wp-content/uploads/2020/09/Plakat_2021-2-714x1024.jpg" 
         alt="" width="676" height="970" /></a></p>'''
-    data_out = ["http://ragaly.hu/wp-content/uploads/2020/09/Plakat_2021-2.jpg"] 
+    data_out = ["http://adress.hu/wp-content/uploads/2020/09/Plakat_2021-2.jpg"] 
     # data_out = ""
     # tr = HtmlTransformer()
     # print(tr)
--- tests/test_soap/test_transform.py
+++ tests/test_soap/test_transform.py
@@ -9,16 +9,16 @@ def test_transform():
         [14, "Post 2", 10, "<!-- wp:tadv/classic-paragraph -->\n<p><strong>A versenyekről röviden</strong></p>\n<p>Nevezés:", "2022-02-22 22:22:22"],
         [13, "Post 3", 8, 
         '''<p><strong>JuPYther</strong></p> 
-        <p><a href="http://ragaly.hu/wp-content/uploads/2020/09/Plakat_2021-2.jpg">
+        <p><a href="http://adress.hu/wp-content/uploads/2020/09/Plakat_2021-2.jpg">
         <img class="size-large wp-image-42622 aligncenter" 
-        src="http://ragaly.hu/wp-content/uploads/2020/09/Plakat_2021-2-714x1024.jpg" 
+        src="http://adress.hu/wp-content/uploads/2020/09/Plakat_2021-2-714x1024.jpg" 
         alt="" width="676" height="970" /></a></p>''',
          "2022-02-22 22:22:22"]
         ]
     post_out = [
         [15, 'Post 1', 12, '[b]Hello World[/b]', '2017-07-10 16:32:05', []], 
         [14, 'Post 2', 10, '\n[b]A versenyekről röviden[/b]\nNevezés:', '2022-02-22 22:22:22', []],
-        [13, 'Post 3', 8, '[b]JuPYther[/b]\n\n', '2022-02-22 22:22:22', ['http://ragaly.hu/wp-content/uploads/2020/09/Plakat_2021-2.jpg']]
+        [13, 'Post 3', 8, '[b]JuPYther[/b]\n\n', '2022-02-22 22:22:22', ['http://adress.hu/wp-content/uploads/2020/09/Plakat_2021-2.jpg']]
         ]
     test2 = transform.transform(post_in)
     for i in test2:
