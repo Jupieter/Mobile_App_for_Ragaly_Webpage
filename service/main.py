e7a2baaef15b56aa489d4aa058bb1613555e6b55
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-10-04 10:55:30 +0200

    modified service

--- service/main.py
+++ service/main.py
@@ -4,19 +4,25 @@ def start_service():
     from jnius import autoclass, cast
 
     print("1 - start_service")
-    service = autoclass("org.jupieter.ragaly_news.ServiceRagaly")
     mActivity = autoclass("org.kivy.android.PythonActivity").mActivity
-    service.start(mActivity, "")
+    
     try:
         msg_service = autoclass("org.jupieter.ragaly_news.RagalyBroadcastService")
-        print("RagalyBroadcastService:  ",service)
+        print("RagalyBroadcastService:  ", msg_service)
+        try:
+            msg_service.start(mActivity)
+            print("RagalyBroadcastService.started")
+        except:
+            print("NO   msg_service.start")
     except:
         print("NO   RagalyBroadcastService")
-    try:
-        msg_service.start(mActivity)
-        print("RagalyBroadcastService.started")
-    except:
-        print("NO   msg_service.start")
+
+
+    print("2 - start_service")
+    service = autoclass("org.jupieter.ragaly_news.ServiceRagaly")
+    service.start(mActivity, "")
+    print("3 - end_service")
+
     return service
 
 
